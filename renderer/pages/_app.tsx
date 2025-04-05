ec22298c6c095608da2f4d523db57ec0c7b07797
1fworks <1fworks@github.com>
2025-04-05 15:02:12 +0900

    chakra ui

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
 import { Provider } from '@/components/ui/provider'
+import { ColorModeButton } from '@/components/ui/color-mode'
 
 import '../styles/globals.css'
+import { Button } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider>
+      <ColorModeButton className='mx-2 my-2'/>
+      <Button className='my-2 rounded-full'>hi</Button>
       <Component {...pageProps} />
     </Provider>
   )
--- renderer/src/components/ui/provider.tsx
+++ renderer/src/components/ui/provider.tsx
@@ -1,14 +1,25 @@
-"use client"
-
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react"
 import {
   ColorModeProvider,
   type ColorModeProviderProps,
 } from "./color-mode"
 
+import { createSystem, defineConfig, defaultConfig } from "@chakra-ui/react"
+
+const config = defineConfig({
+  globalCss: {
+    html: {
+      scrollbar: 'hidden',
+      colorPalette: 'pink'
+    },
+  },
+})
+
+const system = createSystem(defaultConfig, config)
+
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={system}>
       <ColorModeProvider {...props} />
     </ChakraProvider>
   )
